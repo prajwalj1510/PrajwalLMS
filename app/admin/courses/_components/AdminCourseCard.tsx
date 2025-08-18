b0728b1b69f7ff4c35cb820bc4e4fb44adb7173a
Prajwal <prajwal1510j@gmail.com>
2025-08-18 22:45:23 +0530

    added Display & edit Course functionality

---
+++ app/admin/courses/_components/AdminCourseCard.tsx
@@ -0,0 +1,105 @@
+import { AdminCourseType } from "@/app/data/admin/admin-get-courses"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useConstructUrl } from "@/hooks/use-construct-url"
+import { ArrowRight, Eye, MoreVertical, Pencil, SchoolIcon, TimerIcon, Trash2 } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+interface AdminCourseCardProps {
+    data: AdminCourseType
+}
+
+export const AdminCourseCard = ({ data }: AdminCourseCardProps) => {
+
+    const thumbnailUrl = useConstructUrl(data.fileKey)
+
+    return (
+        <Card className="group relative py-0 gap-y-0">
+            {/* Absolute dropdrown */}
+            <div className="absolute top-2 right-2 z-10">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant='secondary' size='icon'>
+                            <MoreVertical className="size-4"/>
+                        </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent align="end" className="w-48">
+                        <DropdownMenuItem asChild>
+                            <Link href={`/admin/courses/${data.id}/edit`}>
+                                <Pencil className="size-4 mr-2"/>
+                                Edit
+                            </Link>
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem asChild>
+                            <Link href={`/admin/courses/${data.slug}`}>
+                                <Eye className="size-4 mr-2"/>
+                                Preview
+                            </Link>
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem asChild>
+                            <Link href={`/admin/courses/${data.id}/delete`}>
+                                <Trash2 className="size-4 mr-2 text-destructive"/>
+                                Delete
+                            </Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+
+            <Image
+                src={thumbnailUrl}
+                alt="Thumbnail url"
+                width={600}
+                height={400}
+                className="w-full rounded-lg aspect-video h-full object-cover"
+            />
+
+            <CardContent className="p-4">
+                <Link
+                    href={`/admin/courses/${data.id}`}
+                    className="text-lg line-clamp-2 hover:underline group-hover:text-primary transition-colors"
+                >
+                    {data.title}
+                </Link>
+
+                <p className="line-clamp-2 text-sm text-muted-foreground leading-tight mt-2">
+                    {data.smallDescription}
+                </p>
+
+                <div className="mt-4 flex items-center gap-x-5">
+                    <div className="flex items-center gap-x-2">
+                        <TimerIcon className="size-6 p-1 rounded-md text-primary bg-primary/10" />
+                        <p className="text-sm text-muted-foreground">
+                            {data.duration}h
+                        </p>
+                    </div>
+
+                    <div className="flex items-center gap-x-2">
+                        <SchoolIcon className="size-6 p-1 rounded-md text-primary bg-primary/10" />
+                        <p className="text-sm text-muted-foreground">
+                            {data.level}
+                        </p>
+                    </div>
+                </div>
+
+                <Link 
+                    href={`/admin/courses/${data.id}/edit`}
+                    className={buttonVariants({
+                        className: 'w-full mt-4'
+                    })}
+                >
+                    Edit Course <ArrowRight className="size-4"/>
+                </Link>
+            </CardContent>
+        </Card>
+    )
+}
+
+// https://newprajwal.t3.storageapi.dev/4b1c7078-a4b5-44fe-ba97-6b8430dbc83a-photo.jpg
---
+++ app/data/admin/admin-get-courses.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/db"
+import { requireAdmin } from "./require-admin"
+
+export const adminGetCourses = async () => {
+    await requireAdmin()
+
+    const data = await prisma.course.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        },
+        select: {
+            id: true,
+            title: true,
+            smallDescription: true,
+            duration: true,
+            level: true,
+            status: true,
+            price: true,
+            fileKey: true,
+            slug: true,
+        },
+    })
+
+    return data
+}
+
+export type AdminCourseType = Awaited<ReturnType<typeof adminGetCourses>>[0]
